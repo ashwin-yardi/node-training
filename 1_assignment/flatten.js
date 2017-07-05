f335877fb8f7f4b54f503cd94ed05dc4a3b80c57
rails <ashwin.yar@gmail.com>
2017-07-05 20:46:59 +0530

    Identation Changed to 2 spaces :)

--- 1_assignment/flatten.js
+++ 1_assignment/flatten.js
@@ -1,32 +1,32 @@
 //Sample Complex Nested JSON Object.
-data = { "flatJSON": false, "i": { "am": { "not": { "so": { "flat": true, "unflat": false } }, "a": "tree" } }, "dates": [ { "day": 1 }, { "day": 8947 } ] }		
+data = { "flatJSON": false, "i": { "am": { "not": { "so": { "flat": true, "unflat": false } }, "a": "tree" } }, "dates": [ { "day": 1 }, { "day": 8947 } ] }        
 
-function flatten(unflatObject){	
-	var flattened_json = {};
-	function next_object(object , prefix_string){
-		if(typeof object == 'object'){
-			if(Array.isArray(object)){	
-				//Check if Array.
-				for(var i = 0; i < object.length; i++){
-					next_object(object[i], prefix_string + "." + i);	
-				}
-				if (object.length == 0){
-					flattened_json[prefix_string] = [];
-				}
-			}
-			else{	
-				//Check if Object.
-				for (var obj in object){		
-					next_object(object[obj], prefix_string ? prefix_string + "." + obj : String(obj));
-				}
-			}	
-		}
-		else{
-		 	flattened_json[prefix_string] = object;
-		}
-	}
-	next_object(unflatObject, "");
-	return flattened_json;
+function flatten(unflatObject){ 
+  var flattened_json = {};
+  function next_object(object , prefix_string){
+    if(typeof object == 'object'){
+        if(Array.isArray(object)){  
+          //Check if Array.
+          for(var i = 0; i < object.length; i++){
+              next_object(object[i], prefix_string + "." + i);    
+          }
+          if (object.length == 0){
+              flattened_json[prefix_string] = [];
+          }
+      }  
+      else{   
+        //Check if Object.
+        for (var obj in object){        
+            next_object(object[obj], prefix_string ? prefix_string + "." + obj : String(obj));
+        }
+      }   
+    }
+    else{
+      flattened_json[prefix_string] = object;
+    }
+  }
+  next_object(unflatObject, "");
+  return flattened_json;
 }
 
 flatten(data);
--- 1_assignment/unflatten.js
+++ 1_assignment/unflatten.js
@@ -2,41 +2,41 @@
 var data = {"flatJSON": false, "i.am.not.so.flat": true, "i.am.not.so.unflat": false, "i.am.a": "tree", "dates.0.day": 1, "dates.1.day": 8947};
 
 function unflatten(flatObject){
-	var temp_str;
-	var unflatObject = {};
-	for (obj in flatObject){	
-		var temp_object = {};
-		temp_str = String(obj);
-		var splitted_string = temp_str.split(".");
-		// call to form_object function with the copy of reference for unflatObject.
-		form_object(unflatObject , splitted_string , flatObject[obj]);
-	}		
-	return unflatObject;
+  var temp_str;
+  var unflatObject = {};
+  for (obj in flatObject){  
+    var temp_object = {};
+    temp_str = String(obj);
+    var splitted_string = temp_str.split(".");
+    // call to form_object function with the copy of reference for unflatObject.
+    form_object(unflatObject , splitted_string , flatObject[obj]);
+  }   
+  return unflatObject;
 }
 
 function form_object(unflatObject , splitted_string , value){
-	//as copy of reference for unflatobject is sent in this function , changing the value of unflatObject in this function 
-	//won't affect the value of original unflat object from the caller function.
-	key_length = splitted_string.length -1;
-	for(var i = 0 ; i < key_length ; ++i){	
-		var key = splitted_string[i];  
-		if(!(key in unflatObject )){
-			if (splitted_string[i + 1].match(/\d+/g)){
-				unflatObject[key] = [];
-			}
-			else{
-				unflatObject[key] = {};
-			}
-		}
-		unflatObject=unflatObject[key];
-	}
-	// reach out the innermost object in nested Json and then write the value.
-	// Last element of the splitted_string is always going to be unique. 
-	unflatObject[splitted_string[key_length]] = value;
+  //as copy of reference for unflatobject is sent in this function , changing the value of unflatObject in this function 
+  //won't affect the value of original unflat object from the caller function.
+  key_length = splitted_string.length -1;
+  for(var i = 0 ; i < key_length ; ++i){  
+    var key = splitted_string[i];  
+    if(!(key in unflatObject )){
+      if (splitted_string[i + 1].match(/\d+/g)){
+        unflatObject[key] = [];
+      }
+      else{
+        unflatObject[key] = {};
+      }
+    }
+    unflatObject=unflatObject[key];
+  }
+  // reach out the innermost object in nested Json and then write the value.
+  // Last element of the splitted_string is always going to be unique. 
+  unflatObject[splitted_string[key_length]] = value;
 }
 
 //final function call to unflatten function.
 //sample complex nested json object.
-var b={"flatJSON": false, "i": { "am": { "not": { "so": { "flat": true, "unflat": false } }, "a": "tree" } }, "dates": [ { "day": 1 }, { "day": 8947 } ] };
+var b = {"flatJSON": false, "i": { "am": { "not": { "so": { "flat": true, "unflat": false } }, "a": "tree" } }, "dates": [ { "day": 1 }, { "day": 8947 } ] };
 
 
