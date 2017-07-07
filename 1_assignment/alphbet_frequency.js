e5f4e1af9ce844d5d2f4dd463a43f87b64722b8f
rails <ashwin.yar@gmail.com>
2017-07-07 19:04:44 +0530

    Updated

---
+++ 1_assignment/alphbet_frequency.js
@@ -0,0 +1,32 @@
+function calculateFrequencyUsingString (string) {
+  var jsonOutput = {};
+  for (var i = 0; i < string.length; i++) {
+    var charAsciiValue = string.charCodeAt(i);
+    if (charAsciiValue >= 97 && charAsciiValue <= 122) {
+      var key = string[i];
+      if (!(key in jsonOutput)) {
+        jsonOutput[key] = 1;
+      } else {
+        jsonOutput[key]++;
+      }
+    }
+  }
+  return jsonOutput;
+}
+  
+function calculateFrequencySimplerLogic (string) {
+  var jsonOutput = {};
+  var flag = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i].match(/[a-z]/i)) {
+      var key = string[i];
+      if (!jsonOutput.hasOwnProperty(string[i])) {
+        jsonOutput[key] = 1;
+        console.log("Ala na");  
+      } else {
+        jsonOutput[key]++;
+      }
+    }
+  }
+  return jsonOutput;
+}
---
+++ 1_assignment/unflatten.js
@@ -0,0 +1,26 @@
+function unflatten (flatObject) {
+  var tempStr;
+  var unflatObject = {};
+  for (obj in flatObject) {  
+    tempStr = String(obj);
+    var splittedString = tempStr.split(".");
+    formObject(unflatObject, splittedString, flatObject[obj]);
+  }   
+  return unflatObject;
+}
+
+function formObject (referencedunflatObject, splittedString, value) {
+  var keyLength = splittedString.length - 1;
+  for (var i = 0; i < keyLength; ++i) {  
+    var key = splittedString[i];  
+    if (!(key in referencedunflatObject)) {
+      if (splittedString[i + 1].match(/\d+/g)) {
+        referencedunflatObject[key] = [];
+      } else {
+        referencedunflatObject[key] = {};
+      }
+    }
+    referencedunflatObject = referencedunflatObject[key];
+  } 
+  referencedunflatObject[splittedString[keyLength]] = value;
+}
