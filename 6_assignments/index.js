023e87930484932578505d3c5c8ebc2b80d30405
rails <ashwin.yar@gmail.com>
2017-07-14 17:34:43 +0530

    API Wrapper without MVC Architecture

--- 6_assignments/index.js
+++ 6_assignments/index.js
@@ -2,21 +2,37 @@ var express = require('express'),
 	app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
+var https = require('https');
+var makeRequest = require('./consumer.js')
 
 var server = app .listen(3000, function() {
 	console.log('Express server is listening at port: ' + 3000);
 });
 
 router.get('/clients', function(request, response) {
-	response.json({Message: "Hooray!!"});
+	makeRequest('GET', undefined, '/clients').then(function(data) {
+		response.json(data);
+	});
 });
 
-router.post('/signin', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
+router.get('/clients/:id', function(request, response) {
+	makeRequest('GET', undefined, '/clients/' + request.params.id, response)
+	 .then(function(data) {
+	 		response.json(data);
+	 });
+});
 
-	response.json({Message: "MC"});
-	response.end();
+router.post('/clients', function(request, response) {
+	makeRequest('POST', request.body, '/clients').then(function(data) {
+		response.json(data);
+	});
+});
+
+router.delete('/clients/:id', function(request, response) {
+	makeRequest('DELETE', undefined, '/clients/' + request.params.id, response)
+	 .then(function(data) {
+	 		response.send(data);		
+	 });
 });
 
 
